64aa29951e5c66bd4212153e8d3ddd9d8fc0b5ee
IndahRahayuisr <108136189+IndahRahayuisr@users.noreply.github.com>
2024-04-18 14:44:03 +0700

    SCRUM-41
    SCRUM-41

--- resources/views/informasidokter/index.blade.php
+++ resources/views/informasidokter/index.blade.php
@@ -1,29 +1,59 @@
-<a href="/informasidokter/create">EDIT Informasi Dokter</a>
-<table border>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<div class="container">
+
+
+<table  class="table table-striped table-hover table-bordered table-white table-striped" border="2">
     <tr>
-        <td>NOMOR</td>
-        <td>NAMA DOKTER</td>
-        <td>NAMA POLI</td>
-        <td>SENIN</td>
-        <td>SELASA</td>
-        <td>RABU</td>
-        <td>KAMIS</td>
-        <td>JUMAT</td>
-        <td>SABTU</td>
-        <td>STATUS KEHADIRAN</td>
+        <th scope="col">NOMOR</th>
+        <th scope="col">NAMA DOKTER</th>
+        <th scope="col">NAMA POLI</th>
+        <th scope="col">SENIN</th>
+        <th scope="col">SELASA</th>
+        <th scope="col">RABU</th>
+        <th scope="col">KAMIS</th>
+        <th scope="col">JUMAT</th>
+        <th scope="col">SABTU</th>
+        <th scope="col">STATUS KEHADIRAN</th>
+        <td></td>
     </tr>
     @foreach($informasidokter as $informasidokter)
     <tr>
-        <td>{{$informasidokter->Nomor}}</td>
-        <td>{{$informasidokter->NamaDokter}}</td>
-        <td>{{$informasidokter->NamaPoli}}</td>
+        <td>{{$informasidokter->id}}</td>
+        <td>{{$informasidokter->Nama_Dokter}}</td>       
+        <td>{{$informasidokter->Nama_Poli}}</td>
         <td>{{$informasidokter->Senin}}</td>
         <td>{{$informasidokter->Selasa}}</td>
         <td>{{$informasidokter->Rabu}}</td>
         <td>{{$informasidokter->Kamis}}</td>
         <td>{{$informasidokter->Jumat}}</td>
         <td>{{$informasidokter->Sabtu}}</td>
-        <td>{{$informasidokter->StatusKehadiran}}</td>
+        <td>{{$informasidokter->Status_Kehadiran}}</td>
+
+        <td>
+            <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+            <a class="btn btn-warning" href="/informasidokter/{{$informasidokter->id}}/edit">Edit</a>
+            <form action="/informasidokter/{{$informasidokter->id}}" method="POST">
+                @csrf
+                @method('delete')
+                <input type="submit" class="btn btn-danger" value="delete">
+            </form>
+            </div>
+            
+        </td>
     </tr>
+
     @endforeach
 </table>
+<a type="button" class="btn btn-success" href="/informasidokter/create">Tambah Informasi Dokter</a>
+</div>
