cb8695600b3f7d0a12bc2da39743e522a2b2bbf2
IndahRahayuisr <108136189+IndahRahayuisr@users.noreply.github.com>
2024-05-23 13:36:42 +0700

    ganti warna & kasih jarak kolom di index

--- resources/views/rumahsakit/indexrumahsakit.blade.php
+++ resources/views/rumahsakit/indexrumahsakit.blade.php
@@ -4,28 +4,29 @@
     <div class="container">
         <br>
         <div class="h2 text-center mt-5 mb-4" style="font-family: Montserrat, sans-serif; font-weight: bold;">
-            Informasi Layanan Poli Klinik Telkomedika
-        </div>
-        <div class="d-flex justify-content-between align-items-center my-3">
-            <a class="btn btn-secondary" href="/rumahsakit/createrumahsakit">Tambah Data Rumah sakit mitra</a>
+            Informasi Rumah Sakit Mitra Klinik Telkomedika
         </div>
         <div class="row">
             <div class="col-md-12">
                 <div class="card shadow mb-4">
                     <div class="card-header py-3" style="background-color: #B6252A;">
+                        <div class="d-flex justify-content-between align-items-center my-3">
                         <h6 class="m-0 font-weight-bold text-white">Daftar Layanan Rumah Sakit Mitra</h6>
+                        <div></div>
+                            <a class="btn btn-secondary ml-auto" href="/rumahsakit/createrumahsakit">Tambah Data Rumah sakit mitra</a>
+                        </div>
                     </div>
                     <div class="card-body mb-4">
                         <div class="row">
                             @foreach ($rumahsakit as $rs)
-                                <div class="col-md-4">
-                                    <div class="card mb-4" style="background-color: #B6252A; height: 100%; border: 0px solid black;">
+                                <div class="col-md-4 mb-4">
+                                    <div class="card shadow-sm" style="background-color:#C0C0C0; height: 100%; border: 0px solid black;">
                                         <div class="card-body text-center">
                                             <img src="{{ asset('storage/' . $rs->Gambar) }}" alt="Hospital Picture" width="120px" height="120px" style="position: absolute; left: 50%; transform: translateX(-50%);">
                                             <h5 class="card-title" style="margin-top: 130px;">{{ $rs->NamaRumahsakit }}</h5>
                                             <p class="card-text">{{ $rs->Alamat }}</p>
                                             <p class="card-text">{{ $rs->Notelepon }}</p>
-                                            <p><a href="{{ $rs->Lokasi }}" target="_blank">Lihat Lokasi</a></p>
+                                            <p><a class="btn btn-primary disabled" role="button" aria-disabled="true" link href="{{ $rs->Lokasi }}" target="_blank">Lihat Lokasi</a></p>
                                         </div>
                                         <div class="btn-group" role="group" aria-label="Basic example" style="padding: 10px;">
                                             <a class="btn btn-warning btn-sm" href="/rumahsakit/{{$rs->id}}/edit">Edit</a>
