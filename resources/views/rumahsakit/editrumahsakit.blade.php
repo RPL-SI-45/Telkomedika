1616f27e4a1bba25d3812573a44e084961e425f3
IndahRahayuisr <108136189+IndahRahayuisr@users.noreply.github.com>
2024-05-23 14:06:41 +0700

    benerin upload gambar di bag edit

--- resources/views/rumahsakit/editrumahsakit.blade.php
+++ resources/views/rumahsakit/editrumahsakit.blade.php
@@ -30,7 +30,7 @@
 
                     <form action="/rumahsakit/store" method="POST" enctype="multipart/form-data">
                         <div class="form-group">
-                            <label for="Gambar">Upload Gambar</label>
+                            <label for="image">Upload Gambar</label>
                             <input type="file" name="Gambar" class="form-control-file" required>
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
