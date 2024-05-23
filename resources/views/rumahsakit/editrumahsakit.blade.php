f27fc3858b651c144c7920a41d8a5af877760027
IndahRahayuisr <108136189+IndahRahayuisr@users.noreply.github.com>
2024-05-23 13:57:14 +0700

    masih ngerapihin kodingan edit

--- resources/views/rumahsakit/editrumahsakit.blade.php
+++ resources/views/rumahsakit/editrumahsakit.blade.php
@@ -5,13 +5,13 @@
         <div class="h2 text-center mt-5 mb-4" style="font-family: Montserrat, sans-serif; font-weight: bold;">Informasi Layanan Rumah Sakit Mitra Klinik Telkomedika</div>
         <div class = "card">
             <div class = "card-body">
-        <form action="/rumahsakit/{{$rumahsakit->id}}" method="POST" enctype="multipart/form-data">
-        @method('put')
-        @csrf
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Nama Rumahsakit</label>
-                    <input type="text" name="NamaRumahsakit" class="form-control" id="exampleFormControlInput1" value="{{$rumahsakit->NamaRumahsakit}}">
-                </div>
+                <form action="/rumahsakit/{{$rumahsakit->id}}" method="POST" enctype="multipart/form-data">
+                    @method('put')
+                    @csrf
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Nama Rumahsakit</label>
+                        <input type="text" name="NamaRumahsakit" class="form-control" id="exampleFormControlInput1" value="{{$rumahsakit->NamaRumahsakit}}">
+                    </div>
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Alamat</label>
                     <input type="text" name="Alamat" class="form-control" id="exampleFormControlInput1" value="{{$rumahsakit->Alamat}}">
