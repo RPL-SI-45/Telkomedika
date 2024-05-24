f1a271e69b4d36a0b486595db79a073c7518f7a4
IndahRahayuisr <108136189+IndahRahayuisr@users.noreply.github.com>
2024-05-24 16:17:15 +0700

    lihatRumahSakit (Pasien)

--- resources/views/lihatrumahsakit/indexlihatrs.blade.php
+++ resources/views/lihatrumahsakit/indexlihatrs.blade.php
@@ -1,29 +1,37 @@
 @extends('layoutrumahsakit.rs')
 
-
 @section('main-section')
-    <div class = "container"> </br>
-    <div class="h2 text-center mt-5 mb-4" style="font-family: Montserrat, sans-serif; font-weight: bold;">Informasi Layanan Poli Klinik Telkomedika</div>
+    <div class="container">
+        <br>
+        <div class="h2 text-center mt-5 mb-4" style="font-family: Montserrat, sans-serif; font-weight: bold;">
+            Informasi Rumah Sakit Mitra Klinik Telkomedika
+        </div>
         <div class="row">
-        <div class="col-md-12">
-            <div class="card shadow mb-4">
-                <div class="card-header py-3"; style="background-color: #B6252A";>
-                    <h6 class="m-0 font-weight-bold text-white">Daftar Layanan Rumah Sakit Mitra</h6>
-                </div>
-                <br>
-                <div class="row">
-        @foreach ($rumahsakit as $rs)
-        <div class="col-md-4">
-            <div class="card mb-4" style="background-color: #B6252A; width: 100%; height: 220px; border: 0px solid black;">
-                <div class="card-body text-center">
-                    <img src="{{ $rs-> Gambar}}" alt="Hospital Picture" width="120px" height="120px" style="position: absolute; left: 20%; transform: translateX(-50%);">
-                    <h5 class="card-title">{{ $rs->NamaRumahsakit }}</h5>
-                    <p class="card-text">{{ $rs->Alamat }}</p>
-                    <p class="card-text">{{ $rs->Notelepon }}</p>
-                    <p><a href="{{ $rs->Lokasi }}" target="_blank">Lihat Lokasi</a></p>
+            <div class="col-md-12">
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3" style="background-color: #B6252A;">
+                        <div class="d-flex justify-content-between align-items-center my-3">
+                        <h5 class="m-0 font-weight-bold text-white">Daftar Rumah Sakit Mitra</h5>
+                    </div>
+                    <div class="card-body mb-4">
+                        <div class="row">
+                            @foreach ($rumahsakit as $rs)
+                                <div class="col-md-4 mb-4">
+                                    <div class="card shadow-sm" style="background-color:#C0C0C0; height: 100%; border: 0px solid black;">
+                                        <div class="card-body text-center">
+                                            <img src="{{ asset('storage/' . $rs->Gambar) }}" alt="Hospital Picture" width="120px" height="120px" style="position: absolute; left: 50%; transform: translateX(-50%);">
+                                            <h5 class="card-title" style="margin-top: 130px;">{{ $rs->NamaRumahsakit }}</h5>
+                                            <p class="card-text">{{ $rs->Alamat }}</p>
+                                            <p class="card-text">{{ $rs->Notelepon }}</p>
+                                            <a link href="{{ $rs->Lokasi }}" target="_blank" class="btn btn-primary" role="button" aria-disabled="true">Lihat Lokasi</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-        @endforeach
 @endsection
