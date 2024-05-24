01ba725b60e43bee675f1e0573c4e02d1d38827a
febyanahutauruk <febyhutauruk40@gmail.com>
2024-05-24 14:32:39 +0700

    CRUM-39 Riwayat Berobat Final

--- resources/views/record/index.blade.php
+++ resources/views/record/index.blade.php
@@ -24,33 +24,40 @@
         }
     </style>
 </head>
-    </br>
 <body>
+
 <div class="container">
-    <h2><center>Riwayat Berobat</center></h2>
-    </br>
-    <form>
-        <div class="form-group">
-            <label for="name">Nama:</label>
-            <input type="text" class="form-control" id="name" value="Cindy P Gandis Ayu">
+    <div class="col-md-12">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3" style="background-color: #B6252A;">
+                <h3 class="text-center" style="color: white;">Riwayat Berobat</h3>
+            </div>
+            <div class="card-body">
+                <form>
+                    <div class="form-group">
+                        <label for="name">Nama:</label>
+                        <input type="text" class="form-control" id="name" value="Cindy P Gandis Ayu">
+                    </div>
+                    <div class="form-group">
+                        <label for="date_of_visit">Tanggal Berobat:</label>
+                        <input type="text" class="form-control" id="date_of_visit" value="22 April 2023">
+                    </div>
+                    <div class="form-group">
+                        <label for="health_condition">Kondisi Kesehatan:</label>
+                        <textarea class="form-control" id="health_condition" rows="3" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="diagnosis">Diagnosis:</label>
+                        <textarea class="form-control" id="diagnosis" rows="3" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id metus eu libero pretium interdum. Nulla facilisi.</textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="treatment">Pengobatan:</label>
+                        <textarea class="form-control" id="treatment" rows="3" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id metus eu libero pretium interdum. Nulla facilisi.</textarea>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="date_of_visit">Tanggal Berobat:</label>
-            <input type="text" class="form-control" id="date_of_visit" value="22 April 2023">
-        </div>
-        <div class="form-group">
-            <label for="health_condition">Kondisi Kesehatan:</label>
-            <textarea class="form-control" id="health_condition" rows="3" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</textarea>
-        </div>
-        <div class="form-group">
-            <label for="diagnosis">Diagnosis:</label>
-            <textarea class="form-control" id="diagnosis" rows="3" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id metus eu libero pretium interdum. Nulla facilisi.</textarea>
-        </div>
-        <div class="form-group">
-            <label for="treatment">Pengobatan:</label>
-            <textarea class="form-control" id="treatment" rows="3" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id metus eu libero pretium interdum. Nulla facilisi.</textarea>
-        </div>
-    </form>
+    </div>
 </div>
 
 </body>
